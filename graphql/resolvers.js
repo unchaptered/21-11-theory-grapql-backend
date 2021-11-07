7a1197c053dbe2649e451790ed67490567ef846f
Unchaptered <workstation19961002@gmail.com>
2021-11-08 06:42:16 +0900

    Associated YTN API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,17 +3,22 @@
     우리가 Node.js 에서 이를 사용하기 위해서는
     어떠한 방식으로 Resolve(해결)해야한다.
 */
-import { getMovies, getById, addMovie, deleteMovie } from "./db.js";
+// import { getMovies, getById, addMovie, deleteMovie } from "./db.js";
+
+import { getMovies } from "./yts.js";
+
+console.log("resolvers.js on");
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    // movies: () => getMovies(),
+    // movie: (_, { id }) => getById(id),
   },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
-  },
+  // Mutation: {
+  //   addMovie: (_, { name, score }) => addMovie(name, score),
+  //   deleteMovie: (_, { id }) => deleteMovie(id),
+  // },
 };
 
 export default resolvers;
